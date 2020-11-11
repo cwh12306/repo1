965944cede41f65dc5846ad6c7549a00a0b14798
cwh <cwh@jisad.com>
2020-11-11 14:20:28 +0800

    添加了一个java工程

---
+++ GitTest/src/main/java/cn/sxt/Hello.java
@@ -0,0 +1,12 @@
+package cn.sxt;
+
+/**
+ * @author 11549
+ *@Data 2020年11月11日下午2:10:57
+ *Git使用
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("hello git");
+	}
+}
